1377b09f261556d24736f10c0b0431ca3dd77f3b
Alton88 <altonyee@gmail.com>
2016-10-16 19:07:32 -0500

    Added 2 classes

---
+++ Linq Query/Problem1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Query
+{
+    public class Problem1
+    {
+        private List<string> listOfStrings;
+
+        public Problem1() {
+            listOfStrings = new List<string>() { "Hello", "Hello", "World", "!!!", "!!!" };
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine("Problem 1");
+            Console.WriteLine("---------\n");
+
+            Console.Write("Original list of strings: ");
+
+            foreach(string value in listOfStrings){
+                Console.Write($"{value} ");
+            }
+
+            Console.Write("\nNew list of strings without duplicates: ");
+
+            var result = listOfStrings.Distinct();
+
+            foreach (string value in result)
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.WriteLine("\n");
+        }
+        
+}
+}
--- Linq Query/Program.cs
+++ Linq Query/Program.cs
@@ -10,46 +10,13 @@ namespace Linq_Query
     {
         static void Main(string[] args)
         {
-            //Problem #1
-            //List<string> listOfStrings = new List<string>() { "Hello", "Hello", "World", "!!!", "!!!" };
+            Problem1 problem1 = new Problem1();
+            problem1.DisplayResults();
 
-            //var result = listOfStrings.Distinct();
-            //foreach (string value in result)
-            //{
-            //    Console.Write($"{value} ");
-            //}
-
-            //Problem #2
-            List<string> listOfStings = new List<string>() { "90,100,82,89,55", "100,100,100,90,65", "100,80,100,100,85" };
-            List<double> listOfAverages = new List<double>();
-
-            foreach (string list in listOfStings)
-            {
-                string[] arrayOfStringGrades = list.Split(',');
-                var listOfDoubleGrades = arrayOfStringGrades.Select(x => ToDouble(x)).ToList();
-                listOfDoubleGrades.Remove(listOfDoubleGrades.Min());
-                listOfAverages.Add(listOfDoubleGrades.Average());
-            }
-
-            foreach (double avg in listOfAverages)
-            {
-                Console.WriteLine($"Each Average: {avg:0.0}");
-            }
-
-            Console.WriteLine($"\nTotal Average: {listOfAverages.Average():0.0}");
+            Problem2 problem2 = new Problem2();
+            problem2.DisplayResults();
 
             Console.Read();
-    }
-        public static double ToDouble(string s)
-        {
-            try
-            {
-                return double.Parse(s);
-            }
-            catch
-            {
-            }
-            return 0;
-        }
+        }        
     }
 }
